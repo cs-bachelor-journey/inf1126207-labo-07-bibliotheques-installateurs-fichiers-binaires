2c9e230d4a8c992382c4a630e9fd60af8b443a4c
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-03-17 11:48:23 -0400

    feat: Add TemperatureLib project with conversion methods
    Introduce a new TemperatureLib .NET library (net9.0) that provides CelsiusToFahrenheit and FahrenheitToCelsius utility methods. Includes TemperatureLib.cs, project file with ImplicitUsings and Nullable enabled, solution file, and global.json specifying SDK version 9.0.0.

---
+++ mise_en_application_03/TemperatureLib/TemperatureLib.cs
@@ -0,0 +1,14 @@
+﻿namespace TemperatureLib;
+
+public static class TemperatureLib
+{
+    public static double CelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double FahrenheitToCelsius(double fahrenheit)
+    {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
